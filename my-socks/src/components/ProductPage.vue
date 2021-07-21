bcc715a86f05ac0054bde66596aab760133de14b
RiGun-K <1234suho@naver.com>
2021-07-21 16:09:25 +0900

    my_socks practice

---
+++ my-socks/src/components/ProductPage.vue
@@ -0,0 +1,30 @@
+<!-- 상품 정보 페이지와 카트를 포함하는 페이지 -->
+<template>
+    <div id="app">
+        <div class="nav-bar"></div>
+        <div class="cart">
+            <p>Cart({{ cart.length }})</p>
+        </div>
+        <Product @add-to-cart="addToCart"></Product>
+    </div>
+</template>
+
+<script>
+import Product from './Product.vue';
+// 같은 폴더에 있으므로  ./ 으로 , 혹은 @/ 로 경로를 적어주기 
+
+export default{
+    name:'ProductPage',
+    components:{
+        Product
+    },
+    data() {
+        return {cart:[]}
+    },
+    methods:{
+        addToCart(variantId){
+            this.cart.push(variantId)
+        }
+    }
+}
+</script>
