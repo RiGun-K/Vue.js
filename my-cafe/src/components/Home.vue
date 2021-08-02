800fa2cf2573f633e54303303e6295f910b21d68
RiGun-K <1234suho@naver.com>
2021-08-02 15:20:08 +0900

    Vuex

--- my-cafe/src/components/Home.vue
+++ my-cafe/src/components/Home.vue
@@ -1,11 +1,28 @@
 <template>
     <div id="app">
-        <h1>Home {{$store.state.userId}}</h1>
+        <h1>Welcome {{ $store.state.userId }}</h1>
+        <input v-model="newId" type="text"><button @click="updateUserId">Save</button><br>
+        <button @click="updateReviews">Update</button>
+        <ul>
+            <li v-for="r in $store.state.reviews" :key="r.id">
+                <p>{{ r.body }}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-export default {
-    name:'Home'
-};
-</script> 
+export default{
+    data(){
+        return { newId:''}
+    },
+    methods:{
+        updateUserId(){
+            this.$store.commit('updateUserId',this.newId)
+        },
+        updateReviews(){
+            this.$store.dispatch('getReviews')
+        }
+    }
+}
+</script>
--- my-cafe/src/store/store.js
+++ my-cafe/src/store/store.js
@@ -1,10 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state:{
-        userId:'user'
+        userId:'user',
+        reviews:[]
+    },
+    mutations:{
+        updateUserId(state, newId){
+            state.userId = newId;
+        },
+        updateReviews(state, reviews){
+            state.reviews = reviews
+        }
+    },
+    actions:{
+        getReviews({commit}){
+            axios.get('/api/comments')
+            .then(response=>{
+                commit('updateReivews',response.date)
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        }
     }
 });
