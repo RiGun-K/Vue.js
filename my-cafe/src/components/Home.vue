9818f1a41ac71557708eba783533d6016fe479ec
RiGun-K <1234suho@naver.com>
2021-08-02 16:15:59 +0900

    Map

--- my-cafe/src/components/Home.vue
+++ my-cafe/src/components/Home.vue
@@ -1,11 +1,11 @@
 <template>
     <div id="app">
-        <h1>Welcome {{ $store.state.user.userId }}</h1>
-        <input v-model="newId" type="text"><button @click="updateUserId">Save</button><br>
+        <h1>Welcome {{ userId }}</h1>
+        <input v-model="newId" type="text"><button @click="saveUserId">Save</button><br>
         <button @click="updateReviews">Update</button>
-        <h1>{{ $store.getters.reviewCount }}</h1>
+        <h1>{{ reviewCount }}</h1>
         <ul>
-            <li v-for="r in $store.state.reviews.reviews" :key="r.id">
+            <li v-for="r in reviews" :key="r.id">
                 <p>{{ r.body }}</p>
             </li>
         </ul>
@@ -13,17 +13,31 @@
 </template>
 
 <script>
+import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
 
 export default{
     data(){
         return { newId:''}
     },
+    computed:{
+        // namespaced 가 설정되지 않은 경우
+        ...mapState({
+            userId:state=>state.user.userId
+        }),
+        // namespaced 가 설정 된 경우 모듈명을 적는다.
+        ...mapState('reviews', {
+            reviews:state=>state.reviews
+        }),
+        ...mapGetters('reviews', ['reviewCount'])
+    },
     methods:{
-        updateUserId(){
-            this.$store.commit('updateUserId',this.newId)
+        ...mapMutations(['updateUserId']),
+        ...mapActions('reviews', ['getReviews']),
+        saveUserId(){
+            this.updateUserId(this.newId)
         },
         updateReviews(){
-            this.$store.dispatch('getReviews')
+            this.getReviews()
         }
     }
 }
