e26c593f558c7878534dabe77690632bcf65d339
RiGun-K <1234suho@naver.com>
2021-08-04 16:14:10 +0900

    Node

--- Node/test.js
+++ Node/test.js
@@ -3,7 +3,7 @@ const http = require('http')
 // export는 하나하나 외부로 나가는 public 느낌
 
 const server = http.createServer((req, res)=> {
-    res.end('Hello World')
+    res.end('Hello World, i am RiGun-K')
 })
 // http에 해당하는 createServer 호출  (=>  = 람다식) 
 // res = 응답 , req = 요청 
