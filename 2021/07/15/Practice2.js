8d8a1b5b6edc2688e8be360572d94cf96abe5edc
RiGun-K <1234suho@naver.com>
2021-08-11 16:00:23 +0900

    2021/08/11

---
+++ 2021/07/15/Practice2.js
@@ -0,0 +1,18 @@
+Vue.component('add', {
+    template:'<button @click="add">{{ count }}</button>',
+    data() {
+        return {count:0}
+    },
+    methods:{
+        add(){
+            this.count += 1
+        }
+    }
+})
+
+let app = new Vue({
+    el:'#app',
+    data:{
+        totalCount:0
+    }
+});
