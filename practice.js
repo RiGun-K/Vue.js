147d56aad908f320768ad54b90b99196ccd1122a
RiGun-K <1234suho@naver.com>
2022-01-11 16:02:43 +0900

    2022/01/11

--- practice.js
+++ practice.js
@@ -34,7 +34,9 @@ function ask(question, yes, no) {
 
 ask(
     "동의하십니까?",
-    function() { alert("동의하셨습니다.");},
-    function() { alert("취소 버튼을 누르셨습니다.");}
+    function() { alert("동의하셨습니다.")},
+    function() { 
+        self.opener = self;
+        window.close(); }
 );
 
