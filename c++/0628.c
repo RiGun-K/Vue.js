9ce7fa433d0af58475ff81a3dc192a7af18574d8
KIM RI GUN <80092311+RiGun-K@users.noreply.github.com>
2022-06-28 15:34:23 +0900

    C++ 환경설정

---
+++ c++/0628.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char a = 'A';
+    int b = 200;
+    long c = 400000;
+    float e = 3.1492;
+    char f[10] = "김리건";
+
+    printf("Hello World!");
+    return 0; 
+
+    
+}
