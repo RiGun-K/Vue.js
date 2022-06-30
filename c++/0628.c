51725d4b9ed18bb7d495d5de2e9077d77f140991
KIM RI GUN <80092311+RiGun-K@users.noreply.github.com>
2022-06-30 09:44:37 +0900

    2022/06/30

--- c++/0628.c
+++ c++/0628.c
@@ -8,8 +8,21 @@ int main()
     float e = 3.1492;
     char f[10] = "김리건";
 
-    printf("Hello World!");
-    return 0; 
+    printf("%c %d\n", a,a);
 
-    
+    printf("%d\n", b);
+
+    printf("%lld\n", c);
+
+    //
+
+     int a,b;
+
+    printf("두 수를 입력하시오.");
+    scanf("%d%d", &a,&b);
+
+    printf("%d\n", a*b);
+    return 0;
+
+ 
 }
